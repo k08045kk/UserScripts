6961b0b9887256f8b0a1e0972e03fa5512975d01
toshi <k08045kk@gmail.com>
2020-04-28 00:00:00 +0900

    update v0.1.1

--- RejectServiceWorker/RejectServiceWorker.user.js
+++ RejectServiceWorker/RejectServiceWorker.user.js
@@ -1,51 +1,48 @@
 // ==UserScript==
 // @name         RejectServiceWorkers.user.js
 // @description  Reject to register a ServiceWorker.
-//               Uninstall ServiceWorker that has been installed.
-//               If ServiceWorker was installed, it clears the cache.
+//               Unregister the registered Service Worker.
+//               If ServiceWorker was registered, it clears the cache.
 //               You can use it as a whitelist by setting @exclude.
+//               It is not possible to completely reject registration at the timing of executing the user script.
+// @run-at       document-start
 // @include      https://*/*
 // @exclude      https://example.com/*
-// @version      0.1.0
 // @author       toshi (https://github.com/k08045kk)
 // @license      MIT License
 // @see          https://opensource.org/licenses/MIT
+// @version      0.1.1
+// @see          0.1.0 - 20200328 - 初版
+// @see          0.1.1 - 20200415 - 修正
+// @see          https://www.bugbugnow.net/2020/03/Reject-to-register-a-ServiceWorker.html
 // @grant        none
 // ==/UserScript==
 
 (function() {
   // Reject to register a ServiceWorker
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register = function(scriptURL, options) {
-      return new Promise(function(resolve, reject) {
-        reject(new Error('Reject to register a ServiceWorker.'));
+    ServiceWorkerContainer.prototype.register = function(scriptURL, options) {
+      return new Promise((resolve, reject) => {
         //console.log('Reject to register a ServiceWorker.');
+        reject(new Error('Reject to register a ServiceWorker.'));
       });
     };
   }
   
-  // Uninstalling an Installed ServiceWorker
-  const cacheDelete = function() {
-    if ('caches' in window) {
-      window.caches.keys().then(function(keys) {
-        Promise.all(keys.map((key) => { return window.caches.delete(key); })).then(() => {
-          //console.log('caches delete.');
+  // Unregister the registered Service Worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      for (let i=0; i<registrations.length; i++) {
+        registrations[i].unregister();
+        //console.log('ServiceWorker unregister.');
+      }
+      if (registrations.length != 0) {
+        window.caches.keys().then((keys) => {
+          Promise.all(keys.map((key) => { return window.caches.delete(key); })).then(() => {
+            //console.log('caches delete.');
+          });
         });
-      });
-    }
-  };
-  const unregister = function() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        for(let registration of registrations) {
-          registration.unregister();
-          //console.log('ServiceWorker unregister.');
-          
-          cacheDelete();
-        }
-      });
-    }
-  };
-  window.addEventListener('load', unregister);
-  window.addEventListener('beforeunload', unregister);
+      }
+    });
+  }
 })();
