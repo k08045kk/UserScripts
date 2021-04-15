68f2d791c661248764935bf465b8d8c232e424a5
toshi <k08045kk@gmail.com>
2021-04-16 08:58:11 +0900

    update v6 HTTPS対応 + 文章分割を改善

--- なろう棒読みちゃんボタン/なろう棒読みちゃんボタン.user.js
+++ なろう棒読みちゃんボタン/なろう棒読みちゃんボタン.user.js
@@ -1,33 +1,24 @@
 // ==UserScript==
 // @name        なろう棒読みちゃんボタン
 // @description 小説家になろうを棒読みちゃんを使用して朗読する。
-// @description 棒読みちゃん用のWebSocket受付プラグインの導入が必要です。
-// @include     http://ncode.syosetu.com/*/*
-// @include     https://ncode.syosetu.com/*/*
-// @include     http://novel18.syosetu.com/*/*
-// @include     https://novel18.syosetu.com/*/*
+//              棒読みちゃん用のWebSocket受付プラグインの導入が必要です。
+// @match       *://ncode.syosetu.com/*/*
+// @match       *://novel18.syosetu.com/*/*
 // @author      toshi (https://github.com/k08045kk)
-// @license     MIT License
-// @see         https://opensource.org/licenses/MIT
-// @version     5
-// @note        1.20180210 - 初版
-// @note        2.20180423 - httpsからhttpへの遷移の記述をコメントアウト状態で追加
-// @note        3.20180509 - リファクタリング
-// @note        4.20190226 - 棒読みちゃんへの転送を遅延する（長時間待機後に連続転送でエラーするため）
-// @note        5.20201218 - 朗読内容を微調整（タイトルなし、強調ルビなし、短編にボタン追加）
-// @note        5.20201218 - fix 自動朗読が動作していない
-// @see         https://github.com/k08045kk/UserScripts
+// @license     MIT License | https://opensource.org/licenses/MIT
+// @version     6
+// @since       1 - 20160210 - 初版
+// @since       2 - 20180423 - httpsからhttpへの遷移の記述をコメントアウト状態で追加
+// @since       3 - 20180509 - リファクタリング
+// @since       4 - 20190226 - 棒読みちゃんへの転送を遅延する(長時間待機後の転送でエラーする)
+// @since       5 - 20201218 - 朗読内容を微調整（タイトルなし、強調ルビなし、短編にボタン追加）
+// @since       5 - 20201218 - fix 自動朗読が動作していない
+// @since       6 - 20210416 - HTTPS対応 + 文章分割を改善
 // @see         https://www.bugbugnow.net/2018/02/blog-post_10.html
 // @grant       none
 // ==/UserScript==
 
 (function() {
-  if (window.location.href.startsWith('https:')) {
-    // httpsの場合、httpに遷移する
-    // 強制遷移したくない場合、下記2行をコメントアウトする
-    window.location.href = 'http'+window.location.href.substring(5);
-    return;
-  }
   if (document.getElementById('novel_honbun') == null) {
     // 対象ページではない時
     return;
@@ -52,7 +43,7 @@
       socket.onopen = function(event) {
         //console.log('Server open.');
         
-        const delim = '<bouyomi>';
+        const delim = "<bouyomi>";
         const speed = -1; // 速度50-200。-1を指定すると本体設定
         const pitch = -1; // ピッチ50-200。-1を指定すると本体設定
         const volume= -1; // ボリューム0-100。-1を指定すると本体設定
@@ -66,7 +57,7 @@
         //console.log('Server error: '+event);
         if (callee.error !== true) {
           callee.error = true;
-          alert('棒読みちゃんへの送信に失敗しました。');
+          alert("棒読みちゃんへの送信に失敗しました。");
         }
       };
       socket.onclose = function(event) {
@@ -74,6 +65,68 @@
       };
     } catch(exception){
       //console.log('Error: '+exception);
+    }
+  };
+  
+  // 文章を分割する
+  function* segmenter(text, segments, max) {
+    segments = segments || ['。','、','.',',','\n'];
+    max = max || 256;
+    
+    const indexs = segments.map(() => 0);
+    const len = text.length;
+    let slen, next, prev = 0;
+    while (prev + max < len) {
+      // 区切り文字で分割する
+      // 分割不可（最大文字数分の文字列を出力する）
+      next = prev + max;
+	    for (let s=0; s<segments.length; s++) {
+        slen = segments[s].length;
+        if (indexs[s] === -1) {
+          // 区切り文字なし（捜索済み）
+          continue;
+        } else if (prev < indexs[s]) {
+          if (indexs[s]+slen - prev < max) {
+            // 区切り文字を範囲内で発見（捜索済み）
+            next = indexs[s];
+            break;
+          } else {
+            // 区切り文字を範囲外で発見（捜索済み）
+            continue;
+          }
+        } else if ((indexs[s]=text.indexOf(segments[s], prev)) === -1) {
+          // 対象文字なし
+          continue;
+        } else if (indexs[s]+slen - prev < max) {
+          // 区切り文字を範囲内で発見
+          next = indexs[s];
+          while (true) {
+            // 範囲内で最後の区切り文字まで進める
+            indexs[s] = text.indexOf(segments[s], next+slen);
+            if (indexs[s] === -1) {
+              break;
+            } else if (indexs[s]+slen - prev < max) {
+              next = indexs[s];
+            } else {
+              break;
+            }
+          }
+          break;
+        } else {
+          // 区切り文字を範囲外で発見
+          continue;
+        }
+      }
+      
+      // 分割文字列を出力する
+      if (next + slen <= prev + max) {
+        next += slen;
+      }
+      yield text.substring(prev, next);
+      prev = next;
+    }
+    if (prev < len) {
+      yield text.substring(prev, len);
     }
   };
   
@@ -91,7 +144,7 @@
     
     let text = '';
     
-    // タイトルを追加
+    // タイトル+作者名を追加
     // タイトルなし（長いタイトルを毎回読むのが嫌なため）
     //console.log('title: '+title);
     //text += title + '…。';
@@ -106,7 +159,6 @@
     // 本文を追加
     // ルビあり文字の原文を削除（ルビあり文字を2重に読み上げるのを回避）
     const novel = document.getElementById('novel_honbun').cloneNode(true);
-    //novel.querySelectorAll('ruby rb').forEach(function(v, i, a) { v.innerText = ''; });
     novel.querySelectorAll('ruby rp').forEach(function(v, i, a) { v.innerText = ''; });
     novel.querySelectorAll('ruby').forEach(function(v, i, a) {
       const rb = v.querySelector('rb');
@@ -122,88 +174,77 @@
     });
     text += novel.innerText;
     
-    text += '…。\n以上、棒読みちゃんによる朗読でした。';
-    
+    text += "…。\n以上、棒読みちゃんによる朗読でした。";
+    text = text.replace(/\r?\n\s*/g, '\n')              // 通信量を削減
+               .replace(/[ァ-ヴ][ァ-ヴー・]*/g, '/$&/');// 出力から文字が消える問題対策（例：「ハマれば時間を稼げる」→「ハマればをげる」）
     
     // 一定文字数をこえると、プラグインがインデックス範囲外でハングするため、複数回送信する
-    // 「。」を区切りとして複数回送信する。
     // (文字数だけで区切ると単語の途中で送信してしまう可能性があるため)
     // (単語の途中で送信してしまうと、棒読みちゃんの発音が意図しないものとなる可能性が高い)
-    let len = 200;  // 一回の最大送信文字数
-    let idx = 0;
-    let next, prev = 0;
     let count = 0;
-    while (true) {
-      next = text.indexOf('。', prev);
-      //console.log(next+' '+prev+' '+idx);
-      if (next == -1) { break; }
+    const segments = ['\n\n','。','？','！','、','」', '】','）','…','・','.','?','!',',','}','\n',' '];
+    for (let s of segmenter(text, segments, 200)) {
+      // 休止符の連続使用を制限
+      s = s.replace(/\n\n\n+/g, '\n\n\n')
+           .replace(/・・・+/g, '・・・')
+           .replace(/、、、+/g, '、、、')
+           .replace(/。。。+/g, '。。。')
+           .replace(/……+/g, '……');
+      if (s.trim() == '') { continue; }
       
-      while (next-idx > len) {
-        if (prev == idx) {
-          prev = idx + len;
-        }
-        //console.log(text.substr(idx, prev-idx));
-        sendBouyomiChanWebSocketPlugin(text.substr(idx, prev-idx));
-        // 連続送信すると棒読みちゃん側がエラーするため、遅延させる
-        // 長時間待機後の連続送信で失敗するため、最初のみ待機時間を伸ばす
-        if (count == 0) {
-          await sleep(2500); 
-        } else if (count <= 5) {
-          await sleep(500);
-        } else {
-          await sleep(100);
-        }
-        count++;
-        
-        idx = prev;
+      sendBouyomiChanWebSocketPlugin(s);
+      
+      // 連続送信すると棒読みちゃん側がエラーするため、遅延させる
+      // 長時間待機後の連続送信で失敗するため、最初のみ待機時間を伸ばす
+      if (count == 0) {
+        await sleep(2000); 
+      } else if (count <= 5) {
+        await sleep(500);
+      } else {
+        await sleep(100);
       }
-      prev = next + 1;
+      count++;
     }
-    //console.log(text.substr(idx, text.length-idx));
-    sendBouyomiChanWebSocketPlugin(text.substr(idx, text.length-idx));
     // 一定の文字列を超えた場合、それ以降朗読しなくなる問題がある
     // 例：http://ncode.syosetu.com/n4006r/12/
   };
   
-  // https未対応(対応した場合、if分を削除すること)
-  if ('http:' == document.location.protocol) {
-    // 再生ボタンを配置
-    const element = document.querySelector('.contents1') || document.querySelector('.novel_title');
-    element.innerHTML += `
-<div style="float:right;">
+  // 再生ボタンを配置
+  const element = document.querySelector('.contents1') || document.querySelector('.novel_title');
+  element.innerHTML += `
+<div style="float:right">
   <style>
-    #bouyomichan{
-      cursor: pointer;
-      color: #fff;
-      background: #0076bf;
+    #bouyomichan {
       padding: .25em 1em;
+      background: #0076bf;
+      color: #fff;
+      cursor: pointer;
     }
-    #bouyomichan:disabled{
-      color: #000;
+    #bouyomichan:disabled {
       background: #ccc;
+      color: #000;
     }
   </style>
   <button id="bouyomichan">棒読みちゃん</button>
 </div>`;
-    document.getElementById('bouyomichan').addEventListener('click', onBouyomiChanButton);
+  document.getElementById('bouyomichan').addEventListener('click', onBouyomiChanButton);
     
-    // 自動朗読
-    // URLにbouyomichan=trueを指定するとページ移動で自動朗読する
-    var urlParam = location.search.substring(1);
-    if (urlParam) {
-      var param = urlParam.split('&');
-      var paramArray = [];
-      for (i = 0; i < param.length; i++) {
-        var paramItem = param[i].split('=');
-        paramArray[paramItem[0]] = paramItem[1];
-      }
-      if (paramArray.bouyomichan == 'true') {
-        onBouyomiChanButton.call(document.getElementById('bouyomichan'));
-        
-        document.querySelectorAll('#novel_color .novel_bn a').forEach(function(v) {
-          v.href += '?bouyomichan=true';
-        });
-      }
+  // 自動朗読
+  // URLにbouyomichan=trueを指定するとページ移動で自動朗読する
+  var urlParam = location.search.substring(1);
+  if (urlParam) {
+    var param = urlParam.split('&');
+    var paramArray = [];
+    for (i = 0; i < param.length; i++) {
+      var paramItem = param[i].split('=');
+      paramArray[paramItem[0]] = paramItem[1];
+    }
+    if (paramArray.bouyomichan == 'true') {
+      onBouyomiChanButton.call(document.getElementById('bouyomichan'));
+      
+      document.querySelectorAll('#novel_color .novel_bn a').forEach(function(v) {
+        v.href += '?bouyomichan=true';
+      });
     }
   }
 })();
